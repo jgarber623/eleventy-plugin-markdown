a711adfd8899d89db1af36f528740a6112747fe8
Jason Garber <jason@sixtwothree.org>
2024-01-15 10:58:10 -0500

    Configure ESLint now that we have a test suite

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,10 @@
-module.exports = require('@jgarber/eslint-config');
+const ava = require('@jgarber/eslint-config/ava');
+const config = require('@jgarber/eslint-config');
+
+module.exports = [
+  {
+    ignores: ['coverage']
+  },
+  ...config,
+  ...ava
+];

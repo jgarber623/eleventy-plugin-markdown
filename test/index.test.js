6b45dfda98fc42633eebaeaa17baec4b1da29f86
Jason Garber <jason@sixtwothree.org>
2024-02-20 21:03:08 -0500

    Migrate test suite from AVA to Node.js libraries

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,13 @@
-const test = require("ava");
+const assert = require("node:assert");
+const test = require("node:test");
 
 const Eleventy = require("@11ty/eleventy");
 
-test("default plugin configuration", async (t) => {
+test("default plugin configuration", async () => {
   const eleventy = new Eleventy("test/fixtures/default", null, { configPath: "index.js" });
-  const [{ content }] = await eleventy.toJSON();
+  const results = await eleventy.toJSON();
 
-  t.is(content, `<title>It’s a party!</title>\n<main><p>It’s <em>a</em><br>\nparty!</p>\n<p>Yay!</p>\n</main>\n`);
+  const expected = `<title>It’s a party!</title>\n<main><p>It’s <em>a</em><br>\nparty!</p>\n<p>Yay!</p>\n</main>\n`;
+
+  assert.equal(results[0].content, expected);
 });
